f0488e9deaaa314f521369d0146b6b51872a6b05
Ken <ken@slinky.link>
2022-03-09 19:06:38 +0800

    i hate vercel right now

--- blitz.config.ts
+++ blitz.config.ts
@@ -3,6 +3,18 @@ import { SupportedLocales } from 'types'
 
 export const DEFAULT_LOCAL = SupportedLocales.English
 
+// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`
+}
+
 const config: BlitzConfig = {
   eslint: {
     // TODO: we need to enable this later but eslint packages are being super cringe
