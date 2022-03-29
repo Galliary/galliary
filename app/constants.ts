6c2f01ce97b9c3a0e3d9ce15d6c3b1dcbfe74d35
Ken <ken@slinky.link>
2022-03-29 11:49:42 +0800

    STRAIGHT TO MASTER BABY

--- app/constants.ts
+++ app/constants.ts
@@ -2,7 +2,7 @@ import { UserConnectionType } from 'db'
 
 export const CLOUDFLARE_ACCOUNT_HASH = '8yAzsAP1tGpuA0gIh26SsA'
 
-export const DISCORD_SUPPORT_SERVER_URL = 'https://discord.gg/sry29tvsbh'
+export const DISCORD_SUPPORT_SERVER_URL = 'https://discord.gg/9txsZrHcme'
 
 export const ENABLED_AUTH_STRATEGIES = [
   UserConnectionType.DISCORD,
